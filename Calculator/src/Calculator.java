6983a4c22bb9b58de66047acbc7f1d1d16dbec0d
clara <clara@DESKTOP-K4G5STD.eduroam.wireless.dtu.dk>
2018-03-21 09:35:27 +0100

    Initial commit of the project

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Insert first number: ");
+		Double a = s.nextDouble(); s.nextLine();
+		System.out.println("Insert the operator: ");
+		String op = s.nextLine();
+		System.out.println("Insert seecond number: ");
+		Double b = s.nextDouble();
+		
+		Operation o = Operation.parseOperator(op);
+		if(o == null) {
+			System.err.println("Operator not reconigezed");
+		} else {
+			o.setNumbers(a,b);
+			System.out.println("Result of " + o.getName() + " is " + o.calc());
+		}
+		s.close();
+	}
+
+}
