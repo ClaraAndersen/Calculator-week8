320c8146e6124adb3651b5e593da39f49828ed4a
clara <clara@DESKTOP-K4G5STD.eduroam.wireless.dtu.dk>
2018-03-21 09:41:02 +0100

    Added subtraction

---
+++ Calculator/src/Subtraction.java
@@ -0,0 +1,17 @@
+
+public class Subtraction extends Operation{
+	@Override
+	public String getName() {
+		return "subtraction";
+	}
+	
+	@Override
+	public String getOperator() {
+		return "-";
+	}
+	
+	@Override
+	public double calc() {
+		return a-b;
+	}
+}
